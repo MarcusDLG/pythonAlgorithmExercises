1e26da2ef1c91b2f15b0f5f7ddd5c410c9ecb227
MarcusDLG <delag010@gmail.com>
2020-10-16 13:47:57 -0400

    checking a student solve

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,30 @@
-x = 1
-y = 1
-count = 0
-fib_even_sum = 0
-while count <= 4000000:
-    count = x + y
-    x = y
-    y = count
-    if count % 2 == 0:
-        fib_even_sum += count
-print(fib_even_sum)
+# x = 1
+# y = 1
+# count = 0
+# fib_even_sum = 0
+# while count <= 4000000:
+#     count = x + y
+#     x = y
+#     y = count
+#     if count % 2 == 0:
+#         fib_even_sum += count
+# print(fib_even_sum)
+
+# Sam Hollyer's solve
+# n1 = 0
+# n2 = 1
+# n0 = n1 + n2
+# sums = 0
+# fib_list = []
+# while n0 <= 4000000:
+#     n0 = n1 + n2
+#     n1 = n2
+#     n2 = n0
+#     if n1 % 2 == 0:
+#         fib_list.append(n1)
+#         sums += n1
+# print(sums)
+# print(fib_list)
 
 # Program to display the Fibonacci sequence up to n-th term
 
